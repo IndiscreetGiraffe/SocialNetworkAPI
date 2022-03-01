bcb8953dd664b6b6aae650732d56cba99b7edae3
Kyleah Justice <kikij432@gmail.com>
2022-02-28 21:24:09 -0700

    added the create and update users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,6 +25,24 @@ const userControllers = {
         });
     },
 
+    createUser({ body}, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({ params, body}, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with that id!"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
     
 
 }
