330cb7aa7248f0adfe642ab28e86f7c22b444bca
Kyleah Justice <kikij432@gmail.com>
2022-02-28 20:47:32 -0700

    added teh rest of the schema and the export to this same thought file

--- models/Thought.js
+++ models/Thought.js
@@ -23,3 +23,36 @@ const ReactionSchema = new Schema ({
 }, {_id: false}
 );
 
+const ThoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+    
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
